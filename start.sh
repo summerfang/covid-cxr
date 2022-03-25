8e08ad25e45618b3841bb54b987531bc818dcec2
Summer Fang <summerfang@gmail.com>
2022-03-25 01:15:23 -0700

    Make debug configuration

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-#python -m src.data.preprocess
-
+python -m src.data.preprocess
 python -m src.train
 python -m src.interpretability.lime_explain
 python -m src.interpretability.gradcam
